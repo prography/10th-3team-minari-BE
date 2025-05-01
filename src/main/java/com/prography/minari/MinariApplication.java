5769ba50a11cc859180e507c2caf52a5d84f5c26
jaepyo-Lee <>
2025-05-01 17:08:24 +0900

    init

---
+++ src/main/java/com/prography/minari/MinariApplication.java
@@ -0,0 +1,13 @@
+package com.prography.minari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prography/minari/MinariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prography.minari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
