7d73427d0cf96fa9f2e150d3bb2c62a8feef5551
jaepyo-Lee <>
2025-05-07 00:48:05 +0900

    <feat#1> 기본 엔티티 구성

---
+++ src/main/java/com/prography/minari/answer/Answer.java
@@ -0,0 +1,29 @@
+package com.prography.minari.answer;
+
+import com.prography.minari.common.entity.BaseTimeEntity;
+import com.prography.minari.question.Question;
+import com.prography.minari.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "ANSWERS")
+public class Answer extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String reply;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
