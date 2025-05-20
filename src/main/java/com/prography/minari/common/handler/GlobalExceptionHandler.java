99bc883642d8731b87674bfc4d7d0e192d985be2
isayaksh <isayaksh@gmail.com>
2025-05-20 22:20:09 +0900

    <feat #20> parseClaimsJws 발생할 수 있는 모든 예외처리

--- src/main/java/com/prography/minari/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/prography/minari/common/handler/GlobalExceptionHandler.java
@@ -2,8 +2,15 @@ package com.prography.minari.common.handler;
 
 import com.prography.minari.common.execption.ApiException;
 import com.prography.minari.common.response.ApiResponse;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.security.SignatureException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,6 +18,30 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ApiResponse handleApiException(ApiException e) {
         return ApiResponse.fail();
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity handleExpiredJwtException(ExpiredJwtException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("토큰이 만료되었습니다.");
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity handleSignatureException(SignatureException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("유효하지 않은 서명입니다.");
+    }
+
+    @ExceptionHandler(UnsupportedJwtException.class)
+    public ResponseEntity handleUnsupportedJwtException(UnsupportedJwtException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("지원하지 않는 JWT 포맷입니다.");
+    }
+
+    @ExceptionHandler(MalformedJwtException.class)
+    public ResponseEntity handleMalformedJwtException(MalformedJwtException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("잘못된 JWT 형식입니다.");
     }
 
 }
