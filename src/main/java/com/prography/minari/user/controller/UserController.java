2d2ccfc602e058ed4422e304b38ccdab83621c2f
isayaksh <isayaksh@gmail.com>
2025-05-20 20:23:59 +0900

    <feat #12> user controller '/api/v1' 경로 추가하기

--- src/main/java/com/prography/minari/user/controller/UserController.java
+++ src/main/java/com/prography/minari/user/controller/UserController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.Map;
@@ -20,6 +17,7 @@ import static org.springframework.http.HttpStatus.FOUND;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class UserController {
 
     private final UserService userService;
