d7429da325dd5f5a661ece49dca89eefbb0debe1
isayaksh <isayaksh@gmail.com>
2025-05-24 20:08:22 +0900

    <feat #20> 불필요한 ExceptionHandler 제거

--- src/main/java/com/prography/minari/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/prography/minari/common/handler/GlobalExceptionHandler.java
@@ -21,28 +21,4 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.fail(e.getErrorCode()));
     }
 
-    @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity handleExpiredJwtException(ExpiredJwtException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body("토큰이 만료되었습니다.");
-    }
-
-    @ExceptionHandler(SignatureException.class)
-    public ResponseEntity handleSignatureException(SignatureException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body("유효하지 않은 서명입니다.");
-    }
-
-    @ExceptionHandler(UnsupportedJwtException.class)
-    public ResponseEntity handleUnsupportedJwtException(UnsupportedJwtException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("지원하지 않는 JWT 포맷입니다.");
-    }
-
-    @ExceptionHandler(MalformedJwtException.class)
-    public ResponseEntity handleMalformedJwtException(MalformedJwtException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("잘못된 JWT 형식입니다.");
-    }
-
 }
