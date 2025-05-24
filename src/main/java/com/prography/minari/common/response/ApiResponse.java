4584ba4b41c83a56d2e7ff6e4a637c9cc4b00b04
isayaksh <isayaksh@gmail.com>
2025-05-24 16:50:02 +0900

    <feat #20> ApiResponse 구성 변경

--- src/main/java/com/prography/minari/common/response/ApiResponse.java
+++ src/main/java/com/prography/minari/common/response/ApiResponse.java
@@ -1,48 +1,20 @@
 package com.prography.minari.common.response;
 
-import lombok.Data;
-
-@Data
 public class ApiResponse<T> {
     private Integer code;
-    private String message;
     private T result;
 
     public static <T> ApiResponse<T> success(T result) {
-        return new ApiResponse<>(200, "API 요청이 성공했습니다.", result);
+        return new ApiResponse<>(200, result);
     }
 
-    public static ApiResponse<Void> success() {
-        return success(null);
+    public static <T> ApiResponse<T> fail(Integer code) {
+        return new ApiResponse<>(code, null);
     }
 
-    public static <T> ApiResponse<T> fail(T result) {
-        return new ApiResponse<>(400, "불가능한 요청입니다.", result);
-    }
-
-    public static ApiResponse<Void> fail() {
-        return fail(null);
-    }
-
-    public static <T> ApiResponse<T> error(T result) {
-        return new ApiResponse<>(500, "에러가 발생했습니다.", result);
-    }
-
-    public static <T> ApiResponse<T> unauthorized(T result) {
-        return new ApiResponse<>(401, "인증되지 않은 클라이언트입니다.", result);
-    }
-
-    public static <T> ApiResponse<T> forbidden(T result) {
-        return new ApiResponse<>(403, "권한이 없는 클라이언트입니다.", result);
-    }
-
-    public static ApiResponse<Void> error() {
-        return error(null);
-    }
-
-    private ApiResponse(Integer code, String message, T result) {
+    private ApiResponse(Integer code, T result) {
         this.code = code;
-        this.message = message;
         this.result = result;
     }
+
 }
