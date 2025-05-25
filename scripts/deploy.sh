071205046a97dc962490b5029e9161d659bcc8e2
jaepyo-Lee <>
2025-05-25 10:56:32 +0900

    <deploy #23> deploy.sh 환경변수명 불일치 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
+set -e
 
-echo "ssm에서 애플리케이션 실행시 필요한 설정값 조회"
-export MINARI_DB_URL=$(aws ssm get-parameter --name "/minari/db/url" --with-decryption --query "Parameter.Value" --output text)
-export MINARI_DB_USERNAME=$(aws ssm get-parameter --name "/minari/db/username" --with-decryption --query "Parameter.Value" --output text)
-export MINARI_DB_PASSWORD=$(aws ssm get-parameter --name "/minari/db/pw" --with-decryption --query "Parameter.Value" --output text)
+echo "🔐 SSM에서 설정값 조회"
+export SPRING_DATASOURCE_URL=$(aws ssm get-parameter --name "/minari/db/url" --with-decryption --query "Parameter.Value" --output text)
+export SPRING_DATASOURCE_USERNAME=$(aws ssm get-parameter --name "/minari/db/username" --with-decryption --query "Parameter.Value" --output text)
+export SPRING_DATASOURCE_PASSWORD=$(aws ssm get-parameter --name "/minari/db/pw" --with-decryption --query "Parameter.Value" --output text)
+
 export KAKAO_REST_API_KEY=$(aws ssm get-parameter --name "/minari/oauth/kakao/key" --with-decryption --query "Parameter.Value" --output text)
 export KAKAO_SECRET_KEY=$(aws ssm get-parameter --name "/minari/oauth/kakao/secret" --with-decryption --query "Parameter.Value" --output text)
 export KAKAO_REDIRECT_URI=$(aws ssm get-parameter --name "/minari/oauth/kakao/redirect_uri" --with-decryption --query "Parameter.Value" --output text)
+
 export NAVER_CLIENT_ID=$(aws ssm get-parameter --name "/minari/naver/id" --with-decryption --query "Parameter.Value" --output text)
 export NAVER_CLIENT_SECRET=$(aws ssm get-parameter --name "/minari/naver/secret" --with-decryption --query "Parameter.Value" --output text)
+
 export JWT_SECRET=$(aws ssm get-parameter --name "/minari/jwt/secret" --with-decryption --query "Parameter.Value" --output text)
+
 export ECR_URI=$(aws ssm get-parameter --name "/minari/prod/ECR_URI" --query "Parameter.Value" --output text)
 
-echo "ECR 로그인"
+echo "🔑 ECR 로그인"
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_URI
 
-echo "도커 이미지 pull"
+echo "📥 도커 이미지 Pull"
 docker pull $ECR_URI:latest
 
-echo "기존 컨테이너 종료"
+echo "🧼 기존 컨테이너 정리"
 docker stop minari || true
 docker rm minari || true
 
-echo "컨테이너 실행 (환경변수 주입)"
+echo "🚀 컨테이너 실행 (환경변수 주입)"
 docker run -d --name minari \
-  -e /minari/db/url=$MINARI_DB_URL \
-  -e /minari/db/username=$MINARI_DB_USERNAME \
-  -e /minari/db/pw=$MINARI_DB_PASSWORD \
-  -e /minari/oauth/kakao/key=$KAKAO_REST_API_KEY \
-  -e /minari/oauth/kakao/secret=$KAKAO_SECRET_KEY \
-  -e /minari/oauth/kakao/redirect_uri=$KAKAO_REDIRECT_URI \
-  -e /minari/naver/id=$NAVER_CLIENT_ID \
-  -e /minari/naver/secret=$NAVER_CLIENT_SECRET \
-  -e /minari/jwt/secret=$JWT_SECRET \
+  -e SPRING_DATASOURCE_URL=$SPRING_DATASOURCE_URL \
+  -e SPRING_DATASOURCE_USERNAME=$SPRING_DATASOURCE_USERNAME \
+  -e SPRING_DATASOURCE_PASSWORD=$SPRING_DATASOURCE_PASSWORD \
+  -e KAKAO_REST_API_KEY=$KAKAO_REST_API_KEY \
+  -e KAKAO_SECRET_KEY=$KAKAO_SECRET_KEY \
+  -e KAKAO_REDIRECT_URI=$KAKAO_REDIRECT_URI \
+  -e NAVER_CLIENT_ID=$NAVER_CLIENT_ID \
+  -e NAVER_CLIENT_SECRET=$NAVER_CLIENT_SECRET \
+  -e JWT_SECRET=$JWT_SECRET \
   -p 8080:8080 \
   $ECR_URI:latest
