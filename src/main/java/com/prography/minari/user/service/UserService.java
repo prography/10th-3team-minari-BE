4bc7d003d8abac1c04f3afcfea2c2c130b0346b4
isayaksh <isayaksh@gmail.com>
2025-05-30 20:58:49 +0900

    <feat #38> User service layer 수정

--- src/main/java/com/prography/minari/user/service/UserService.java
+++ src/main/java/com/prography/minari/user/service/UserService.java
@@ -1,7 +1,22 @@
 package com.prography.minari.user.service;
 
 import com.prography.minari.user.dto.UserFindResDto;
+import com.prography.minari.user.entity.User;
+import com.prography.minari.user.service.impl.UserReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserService {
-    UserFindResDto findById(Long id);
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserReader userReader;
+
+    public UserFindResDto findById(Long id) {
+        User user = userReader.read(id);
+        return UserFindResDto.from(user);
+    }
+
 }
