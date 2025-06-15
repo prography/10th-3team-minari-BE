e3b360851d6a7edcaa1bc9903313165c45313eae
jaepyo-Lee <>
2025-06-15 21:30:07 +0900

    <feat #83> 응답값 스웨거 설명 수정

--- src/main/java/com/prography/minari/answer/service/dto/response/InterviewContentResponse.java
+++ src/main/java/com/prography/minari/answer/service/dto/response/InterviewContentResponse.java
@@ -10,18 +10,18 @@ import java.time.LocalDate;
 @Builder
 @Schema(description = "인터뷰 응답 DTO")
 public class InterviewContentResponse {
-    @Schema(description = "답변 내용")
+    @Schema(description = "질문 정답")
     private String answer;
 
-    @Schema(description = "질문 내용")
+    @Schema(description = "질문")
     private String question;
 
-    @Schema(description = "응답 내용")
+    @Schema(description = "사용자 답변")
     private String reply;
 
     @Schema(description = "생성 일자")
     private LocalDate createDate;
 
-    @Schema(description = "러닝 타임 (초)")
+    @Schema(description = "답변 러닝 타임 (초)")
     private Double runningTime;
 }
