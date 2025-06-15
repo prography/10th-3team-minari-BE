3f184543ba5a5823fb5cd236ffd2d29dbe02cab1
isayaksh <isayaksh@gmail.com>
2025-06-15 17:15:40 +0900

    <feat #79> jwt authentication filter 공통 예외 응답값 처리 완료

--- src/main/java/com/prography/minari/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/prography/minari/common/filter/JwtAuthenticationFilter.java
@@ -1,23 +1,39 @@
 package com.prography.minari.common.filter;
 
+import ch.qos.logback.core.util.StringUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prography.minari.common.execption.ApiException;
+import com.prography.minari.common.execption.ErrorCode;
+import com.prography.minari.common.response.CommonResponse;
 import com.prography.minari.common.util.JwtUtil;
 import com.prography.minari.user.entity.User;
 import com.prography.minari.user.service.impl.UserReader;
+import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import static com.prography.minari.common.execption.ErrorCode.JWT_NOT_FOUND_EXCEPTION;
+import static jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -28,22 +44,56 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String token = authHeader.substring(7);
-
-            jwtUtil.isValidateToken(token);
-
-
-            Long userId = Long.parseLong(jwtUtil.getUserId(token));
-            User user = userReader.read(userId); // DB 조회
-            UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(user, null, List.of());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
+        // 로그인 URI는 Filter 처리 생략!
+        if(request.getRequestURI().startsWith("/api/v1/users/oauth/")){
+            filterChain.doFilter(request, response);
+            return;
         }
 
-        filterChain.doFilter(request, response);
+        String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        // 헤더에 Authorization이 존재하지 않을 경우 예외 처리
+        if(StringUtils.isBlank(accessToken)) {
+            log.info("헤더에 Authorization이 존재하지 않습니다. accessToken: {}", accessToken);
+            writeUnauthorizedResponse(response, JWT_NOT_FOUND_EXCEPTION.getCode(), JWT_NOT_FOUND_EXCEPTION.getMessage());
+            return;
+        }
+
+        // JWT 검증
+        try {
+            jwtUtil.isValidateToken(accessToken);
+        } catch (ApiException e) {
+            writeUnauthorizedResponse(response, e.getErrorCode(), e.getErrorMessage());
+            return;
+        }
+
+        // Authentication Context 등록
+        try {
+            Long userId = Long.parseLong(jwtUtil.getUserId(accessToken));
+            User user = userReader.read(userId); // User 조회
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+        } catch (ApiException e) {
+            log.info("{}: {}", e.getErrorCode(), e.getErrorMessage());
+            writeUnauthorizedResponse(response, e.getErrorCode(), e.getErrorMessage());
+        }
+
     }
+
+    // Unauthorized Response 공통 예외 처리 Response
+    private void writeUnauthorizedResponse(HttpServletResponse response, String errorCode, String errorMessage) {
+        try {
+            response.setStatus(SC_UNAUTHORIZED);
+            response.setContentType(APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(new ObjectMapper().writeValueAsString(
+                    CommonResponse.fail(errorCode, errorMessage)
+            ));
+        } catch (IOException e) {
+            log.info("writeUnauthorizedResponse IOException 발생!");
+            throw new RuntimeException(e);
+        }
+    }
+
 }
