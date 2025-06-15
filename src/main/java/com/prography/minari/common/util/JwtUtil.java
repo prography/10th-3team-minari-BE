9152eda32d99117f7ac1da738b195889f87e42ea
isayaksh <isayaksh@gmail.com>
2025-06-15 17:12:20 +0900

    <feat #79> jwtutil log 삭제

--- src/main/java/com/prography/minari/common/util/JwtUtil.java
+++ src/main/java/com/prography/minari/common/util/JwtUtil.java
@@ -51,19 +51,19 @@ public class JwtUtil {
                     .build()
                     .parseClaimsJws(token);
         } catch(ExpiredJwtException e) {
-            log.warn("토큰이 만료되었습니다.");
+            log.info("토큰이 만료되었습니다. JWT: {}", token);
             throw new ApiException(JWT_EXPIRED_EXCEPTION);
         } catch(SignatureException e) {
-            log.warn("유효하지 않은 서명입니다.");
+            log.info("유효하지 않은 서명입니다. JWT: {}", token);
             throw new ApiException(JWT_INVALID_SIGNATURE_EXCEPTION);
         } catch(UnsupportedJwtException e) {
-            log.warn("지원하지 않는 JWT 포맷입니다.");
+            log.info("지원하지 않는 JWT 포맷입니다. JWT: {}", token);
             throw new ApiException(JWT_UNSUPPORT_FORMAT_EXCEPTION);
         } catch(MalformedJwtException e) {
-            log.warn("잘못된 JWT 형식입니다.");
+            log.info("잘못된 JWT 형식입니다. JWT: {}", token);
             throw new ApiException(JWT_WRONG_FORM_EXCEPTION);
         } catch(IllegalStateException e) {
-            log.warn("JWT 파싱 중 예상치 못한 상태 오류가 발생했습니다. 설정 또는 키 값이 올바른지 확인하세요.");
+            log.info("JWT 파싱 중 예상치 못한 상태 오류가 발생했습니다. 설정 또는 키 값이 올바른지 확인하세요. JWT: {}", token);
             throw new ApiException(JWT_EXCEPTION);
         }
     }
