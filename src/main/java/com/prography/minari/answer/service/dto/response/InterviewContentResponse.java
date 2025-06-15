e1aa64522c69cbc6bedd3da1f64594ce0e44656e
jaepyo-Lee <>
2025-06-15 21:15:17 +0900

    <feat #83> stt변환 응답값 스웨거 설명 추가

--- src/main/java/com/prography/minari/answer/service/dto/response/InterviewContentResponse.java
+++ src/main/java/com/prography/minari/answer/service/dto/response/InterviewContentResponse.java
@@ -1,5 +1,6 @@
 package com.prography.minari.answer.service.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,11 +8,20 @@ import java.time.LocalDate;
 
 @Getter
 @Builder
+@Schema(description = "인터뷰 응답 DTO")
 public class InterviewContentResponse {
+    @Schema(description = "답변 내용")
     private String answer;
-    private String question;
-    private String reply;
-    private LocalDate createDate;
-    private Double runningTime;
 
+    @Schema(description = "질문 내용")
+    private String question;
+
+    @Schema(description = "응답 내용")
+    private String reply;
+
+    @Schema(description = "생성 일자")
+    private LocalDate createDate;
+
+    @Schema(description = "러닝 타임 (초)")
+    private Double runningTime;
 }
