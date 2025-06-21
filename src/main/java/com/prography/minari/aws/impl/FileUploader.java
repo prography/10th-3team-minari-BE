2550b1b1e498062e5edd1e5f768342fe1a52928b
jaepyo-Lee <>
2025-06-21 16:48:23 +0900

    <fix #90> s3 업로드 경로수정

--- src/main/java/com/prography/minari/aws/impl/FileUploader.java
+++ src/main/java/com/prography/minari/aws/impl/FileUploader.java
@@ -5,5 +5,5 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface FileUploader {
-    String upload(MultipartFile multipartFile,String folder);
+    String upload(MultipartFile multipartFile,String relativeFolderPath);
 }
