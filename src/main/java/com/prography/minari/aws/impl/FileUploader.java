38bb06bb670d9055d532c45bedc584b8250a8afb
jaepyo-Lee <>
2025-06-21 16:37:44 +0900

    feat: aws s3업로드 추가

---
+++ src/main/java/com/prography/minari/aws/impl/FileUploader.java
@@ -0,0 +1,9 @@
+package com.prography.minari.aws.impl;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface FileUploader {
+    String upload(MultipartFile multipartFile,String folder);
+}
