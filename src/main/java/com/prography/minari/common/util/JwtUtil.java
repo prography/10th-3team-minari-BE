5bcd91de2d865a113ad662f45cd0c5d62ad2ea46
isayaksh <isayaksh@gmail.com>
2025-07-05 01:06:13 +0900

    <hotfix> use-forward-headers: true

--- src/main/java/com/prography/minari/common/util/JwtUtil.java
+++ src/main/java/com/prography/minari/common/util/JwtUtil.java
@@ -43,8 +43,10 @@ public class JwtUtil {
     public ResponseCookie createAccessTokenCookie(String accessToken) {
         return ResponseCookie.from(ACCESS_TOKEN, accessToken)
                 .httpOnly(false) // TODO true로 해야함
-                .secure(true)
-                .domain(".minari-official.com")
+                //.secure(true)
+                //.domain(".minari-official.com")
+                .secure(false)
+                .domain(null)
                 .path("/")
                 .maxAge(Duration.ofMillis(expiration))
                 .sameSite("None")
@@ -67,8 +69,10 @@ public class JwtUtil {
     public ResponseCookie createRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from(REFRESH_TOKEN, refreshToken)
                 .httpOnly(false) // TODO true로 해야함
-                .secure(true)
-                .domain(".minari-official.com")
+                //.secure(true)
+                //.domain(".minari-official.com")
+                .secure(false)
+                .domain(null)
                 .path("/")
                 .maxAge(Duration.ofMillis(expiration * 2))
                 .sameSite("None")
