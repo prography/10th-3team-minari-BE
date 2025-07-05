862ee1b1a9357031dc954362fb2208dea7c64872
isayaksh <isayaksh@gmail.com>
2025-07-05 10:24:27 +0900

    <hotfix> 미래의 나를 위한 리워드 > 미래의 나를 위한 리허설, 인증메일 타이틀 수정

--- src/main/java/com/prography/minari/mail/dto/MailSubject.java
+++ src/main/java/com/prography/minari/mail/dto/MailSubject.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MailSubject {
-    AUTH_SUBJECT("[미래의 나를 위한 리워드] 이메일 인증을 완료해주세요.");
+    AUTH_SUBJECT("[미래의 나를 위한 리허설] 이메일 인증을 완료해주세요.");
     private String name;
 }
