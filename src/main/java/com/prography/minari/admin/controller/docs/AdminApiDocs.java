1af9c68cd17aa8930ce3d25c4a394d7e84d642ad
jaepyo-Lee <>
2025-07-09 23:24:36 +0900

    <fix #104> 스웨거 http method 수정

--- src/main/java/com/prography/minari/admin/controller/docs/AdminApiDocs.java
+++ src/main/java/com/prography/minari/admin/controller/docs/AdminApiDocs.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "어드민 API", description = "어드민 관련 API입니다.")
@@ -16,6 +16,6 @@ public interface AdminApiDocs {
             description = "계좌이체 및 오류 상황시 수기로 씨앗을 지급하는 api"
     )
     @ApiResponse(responseCode = "200", description = "조회 성공")
-    @GetMapping("/admin/api/v1/payment/force/v2")
+    @PostMapping("/admin/api/v1/payment/force/v2")
     ResponseEntity<CommonResponse<String>> forceChargeV2(@RequestBody RewardForceV2Request request);
 }
