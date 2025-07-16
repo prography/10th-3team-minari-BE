4d97a16794b97216f814872921e3c55781f99909
jaepyo-Lee <>
2025-07-17 00:23:29 +0900

    <feat #132> 스케줄링 활성화
    - 스케줄러 config누락 설정
    - jobParam 날짜로 수정

--- src/main/java/com/prography/minari/batch/jobLauncher/MailSendLauncher.java
+++ src/main/java/com/prography/minari/batch/jobLauncher/MailSendLauncher.java
@@ -2,16 +2,23 @@ package com.prography.minari.batch.jobLauncher;
 
 import com.prography.minari.batch.job.MailJobConfiguration;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MailSendLauncher {
@@ -22,9 +29,21 @@ public class MailSendLauncher {
 
     @Scheduled(cron = "0 0 8 * * *")
     public void launch() throws Exception {
-        JobParameters mailSendDateParameter = new JobParametersBuilder()
-                .addLocalDateTime("sendMailDate", LocalDateTime.now())
-                .toJobParameters();
-        jobLauncher.run(mailJobConfiguration.start(jobRepository, transactionManager), mailSendDateParameter);
+        log.info("[{}] Starting mail send job",LocalDateTime.now());
+        try {
+            JobParameters params = new JobParametersBuilder()
+                    .addLocalDate("sendMailDate", LocalDate.now())
+                    .toJobParameters();
+
+            jobLauncher.run(mailJobConfiguration.start(jobRepository, transactionManager), params);
+        } catch (JobInstanceAlreadyCompleteException e) {
+            log.info("Job already completed: {}", e.getMessage());
+        } catch (JobExecutionAlreadyRunningException |
+                 JobRestartException |
+                 JobParametersInvalidException e) {
+            log.error("Job failed to start", e);
+        }finally {
+            log.info("[{}] Finished mail send job",LocalDateTime.now());
+        }
     }
 }
