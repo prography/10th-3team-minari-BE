6ed2b15362fe73181511b0de037377fa56706634
isayaksh <isayaksh@gmail.com>
2025-07-18 16:54:09 +0900

    <hotfix> 계정 활성화 로직 변경

--- src/main/java/com/prography/minari/social/service/SocialService.java
+++ src/main/java/com/prography/minari/social/service/SocialService.java
@@ -50,11 +50,6 @@ public class SocialService {
         User user = userRepository.findBySocialTypeAndSocialId(socialType, userInfoDto.socialId())
                 .orElseGet(() -> userRepository.save(User.create("", socialType, userInfoDto.socialId(), userInfoDto.nickname(), userInfoDto.image(), uuid)));
 
-        // 회원탈퇴한 계정의 경우 예외처리
-        if(user.isDeleted()) {
-            throw new ApiException(ACCOUNT_SOFT_DELETED);
-        }
-
         // jwt 생성
         String serverAccessToken = jwtUtil.createAccessToken(user.getId().toString());
         String serverRefreshToken = jwtUtil.createRefreshToken(user.getId().toString());
