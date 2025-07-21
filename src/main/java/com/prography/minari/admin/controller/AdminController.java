d8c8b6c35bbb4772e1814c6eddb8d49a12db2162
isayaksh <isayaksh@gmail.com>
2025-07-21 19:35:30 +0900

    <faet #130> Admin controller uri 수정

--- src/main/java/com/prography/minari/admin/controller/AdminController.java
+++ src/main/java/com/prography/minari/admin/controller/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/admin/api/v1")
+@RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 public class AdminController implements AdminApiDocs {
     private final AdminRewardService adminRewardService;
