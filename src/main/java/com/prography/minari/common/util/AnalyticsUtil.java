aaa02af65beab527e01208424be5e6bcc36c2da9
isayaksh <isayaksh@gmail.com>
2025-07-27 14:49:18 +0900

    <feat #149> 연속 N일차 달성 값 계산 메소드 추가

--- src/main/java/com/prography/minari/common/util/AnalyticsUtil.java
+++ src/main/java/com/prography/minari/common/util/AnalyticsUtil.java
@@ -4,7 +4,11 @@ import com.prography.minari.answer.dto.res.AnswerResDto;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class AnalyticsUtil {
 
@@ -22,6 +26,23 @@ public class AnalyticsUtil {
     public static int toPercentage(long numerator, long denominator) {
         if (denominator == 0) return 0;
         return Math.toIntExact(Math.round(numerator * 100.0 / denominator));
+    }
+
+    public static int calculateConsecutiveDaysCount(List<AnswerResDto> answers, LocalDate startDate, LocalDate endDate) {
+
+        LocalDate today = LocalDate.now();
+
+        return Math.toIntExact(
+                answers.stream()
+                        .filter(dto -> {
+                            LocalDate date = dto.answerDate();
+                            return !date.isBefore(startDate) && !date.isAfter(today);
+                        })
+                        .sorted(Comparator.comparing(AnswerResDto::answerDate).reversed())
+                        .map(AnswerResDto::isExisted)
+                        .takeWhile(Boolean.TRUE::equals)
+                        .count()
+        );
     }
 
 }
