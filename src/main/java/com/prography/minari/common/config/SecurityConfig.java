8c841797d8f77e584063a88fb0ed258bc63ab28d
isayaksh <isayaksh@gmail.com>
2025-08-03 15:04:37 +0900

    <hotfix> cors error

--- src/main/java/com/prography/minari/common/config/SecurityConfig.java
+++ src/main/java/com/prography/minari/common/config/SecurityConfig.java
@@ -90,7 +90,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("https://minari-official.com", "https://www.minari.shop"));
+        configuration.setAllowedOrigins(List.of(
+                "https://minari-official.com",
+                "https://minari-stg.netlify.app",
+                "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // ✅ 크로스도메인 쿠키 허용
